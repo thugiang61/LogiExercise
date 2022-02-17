0bfd455affc45000d5ae0b89f4bcad8904c9b6b3
Thu Giang <thugiang6.1@gmail.com>
2022-02-17 17:39:34 +0700

    Testing Qusetion component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,29 @@
 <template>
   <div id="app">
+    <h1>Quiz Time!!!</h1>
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div v-for="questionData in myJson" :key="questionData.id">
+      <Question :questionData="questionData" />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Question from "./components/Question.vue";
+import MY_JSON from "@/questions.json";
 
 export default {
+  data: function () {
+    return {
+      myJson: MY_JSON,
+    };
+  },
   name: "App",
   components: {
-    HelloWorld,
+    Question,
   },
+  //custom option named myJson, ko nên đưa vào data
+  // myJson: MY_JSON,
 };
 </script>
 
@@ -23,6 +34,13 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
+  padding: 10%;
+  padding-top: 0;
+  background-color: lightblue;
+}
+
+h1 {
+  font-size: 150px;
 }
 </style>
--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,50 @@
 import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+import Question from "@/components/Question.vue";
 
-describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg },
+describe("UTC Question Component", () => {
+  it("UT001: Test when right answer is chosen", async () => {
+    const wrapper = shallowMount(Question, {
+      propsData: {
+        questionData: {
+          id: 1,
+          question: "Khi gặp Thúy Kiều , Kim Trọng trao cho vật gì làm tin?",
+          answers: [
+            "Dải yếm",
+            "Miếng Lụa",
+            "Chiếc châm cài tóc",
+            "Chiếc khăn hồng",
+          ],
+          rightAnswer: "Chiếc khăn hồng",
+        },
+      },
     });
-    expect(wrapper.text()).toMatch(msg);
+    const rightAnswer = wrapper.findAll("input").at(3);
+    await rightAnswer.trigger("click");
+    expect(wrapper.findAll("p").at(0).text()).toEqual(
+      "Đúng rồi, bạn giỏi quá :33"
+    );
+  });
+
+  it("UT002: Test when wrong answer is chosen", async () => {
+    const wrapper = shallowMount(Question, {
+      propsData: {
+        questionData: {
+          id: 1,
+          question: "Khi gặp Thúy Kiều , Kim Trọng trao cho vật gì làm tin?",
+          answers: [
+            "Dải yếm",
+            "Miếng Lụa",
+            "Chiếc châm cài tóc",
+            "Chiếc khăn hồng",
+          ],
+          rightAnswer: "Chiếc khăn hồng",
+        },
+      },
+    });
+    const wrongAnswer = wrapper.findAll("input").at(1);
+    await wrongAnswer.trigger("click");
+    expect(wrapper.find("span").text()).toEqual(
+      "Tiếc quá, 'Chiếc khăn hồng' mới đúng nha =(("
+    );
   });
 });
